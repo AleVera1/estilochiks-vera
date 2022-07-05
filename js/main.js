a812f34aceb9a1311842edb08da1b7f997de0109
Alejandro Vera <aevlanus@gmail.com>
2022-07-04 22:12:03 -0300

    desafio 3

--- js/main.js
+++ js/main.js
@@ -98,19 +98,25 @@ function showProducts(products) {
     productsContainer.innerHTML = ""
 
     products.forEach(el => {
+      //desestructuración
+      let imagen = el.imagen;
+      let nombre = el.nombre;
+      let precio = el.precio;
+      let id = el.id;
+
       //se crea un div para cada producto
       let productsCard = document.createElement('div')
       productsCard.className = 'product'
       
       productsCard.innerHTML = ` <div class="card text-black card-color" style="width: 18rem;">
                               <div class="card-image">
-                                  <img src="${el.imagen}" width="500px" class="rounded shadow-drop-2-center animate__animated animate__fadeIn img-fluid">
-                                  <span class="card-title text-center pt-3">${el.nombre}</span>
+                                  <img src="${imagen}" width="500px" class="rounded shadow-drop-2-center animate__animated animate__fadeIn img-fluid">
+                                  <span class="card-title text-center pt-3">${nombre}</span>
                                   
                               </div>
                               <div class="card-content">
-                                  <p class= "text-center"> $ ${el.precio}</p>
-                                  <a id="btn${el.id}" class="btn btn-dark buy--btn text-center" role="button">Agregar al Carrito</a>
+                                  <p class= "text-center"> $ ${precio}</p>
+                                  <a id="btn${id}" class="btn btn-dark buy--btn text-center" role="button">Agregar al Carrito</a>
                               </div>
                           </div>
                         </div>`
@@ -121,6 +127,12 @@ function showProducts(products) {
       //boton para agregar al carrito
       btnAdd.addEventListener('click',()=>{
           addToCart(el.id);
+          //alert de la libreria sweetalert que sale cada vez que agregas un producto al carrito
+          Swal.fire({
+            icon: 'success',
+            title: 'Agregado!',
+            text: 'Producto agregado al carrito satisfactoriamente!',
+          })
     })
     })
 }
